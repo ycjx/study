330e9fdb53107e43f30f90b1e006da0b789a82a8
ycjx <897804105@qq.com>
2019-03-02 18:13:10 +0800

    优化冒泡排序

--- src/main/java/com/yxj/sort/BubbleSort.java
+++ src/main/java/com/yxj/sort/BubbleSort.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  */
 public class BubbleSort {
 
-    public static  int[] array = new int[]{23,34,45,12,34,32,13,85,32,4,1,54,24,74,25,86,65,38};
+    public static int[] array = new int[]{23, 34, 45, 12, 34, 32, 13, 85, 32, 4, 1, 54, 24, 74, 25, 86, 65, 38};
 
     public static void main(String[] args) {
 
@@ -17,17 +17,23 @@ public class BubbleSort {
         System.out.println(Arrays.toString(array));
     }
 
-    public static int[] sort(int[] array){
+    public static int[] sort(int[] array) {
 
         int length = array.length;
 
-        for(int i=0;i<length;i++){
-            for(int j=0;j<length-i-1;j++){
-                if(array[j]>array[j+1]){
+        for (int i = 0; i < length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    flag = true;
                     int tmp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = tmp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
                 }
+            }
+            //当发现没有需要交换时 说明排序完成
+            if (!flag) {
+                break;
             }
         }
 
