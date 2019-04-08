ce521fe5188fec4dff2fb552ae2e0e421d4b5920
ycjx <897804105@qq.com>
2019-04-08 10:16:30 +0800

    盛最多的雨水

--- src/main/java/com/yxj/arithmetic/ContainerWithMostWater.java
+++ src/main/java/com/yxj/arithmetic/ContainerWithMostWater.java
@@ -6,4 +6,19 @@ package com.yxj.arithmetic;
  * @create:2019-03-29 17:04
  */
 public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        ContainerWithMostWater c = new ContainerWithMostWater();
+        System.out.println(c.maxArea(new int[]{0}));
+    }
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        int length = height.length;
+        for (int i = 0, j = length - 1; i < j; ) {
+            int minHegth = height[i] < height[j] ? height[i++] : height[j--];
+            max = Math.max(max, (j - i+1) * minHegth);
+        }
+        return max;
+    }
 }
