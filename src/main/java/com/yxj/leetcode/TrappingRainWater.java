56b12f96d79ce7cd84bae49d45fc209804fb27c0
ycjx <897804105@qq.com>
2019-07-10 09:53:31 +0800

    接雨水

--- src/main/java/com/yxj/leetcode/TrappingRainWater.java
+++ src/main/java/com/yxj/leetcode/TrappingRainWater.java
@@ -7,10 +7,64 @@ package com.yxj.leetcode;
  */
 public class TrappingRainWater {
 
+    public static void main(String[] args) {
+        TrappingRainWater trappingRainWater = new TrappingRainWater();
+        int[] water = new int[]{2, 0, 2};
+        System.out.println(trappingRainWater.trap(water));
+    }
+
 
     public int trap(int[] height) {
+        int length = height.length;
+        if (length < 3) {
+            return 0;
+        }
+        int head = 0;
+        int tail = length;
+        int leftLength = height[0];
+        int rightLength = height[length - 1];
+        int value = 0;
+        for (int i = 1; i < length; i++) {
+            if (height[i] >= leftLength) {
+                leftLength = height[i];
+                head = i;
+            } else {
+                break;
+            }
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            if (height[i] >= rightLength) {
+                rightLength = height[i];
+                tail = i;
+            } else {
+                break;
+            }
+        }
+        while (head < tail ) {
+            if (leftLength <= rightLength) {
+                head++;
+                if (head >= length) {
+                    break;
+                }
+                if (height[head] >= leftLength) {
+                    leftLength = height[head];
+                } else {
+                    value = value + (leftLength - height[head]);
+                }
+            } else {
+                tail--;
+                if (tail < 0) {
+                    break;
+                }
+                if (height[tail] > rightLength) {
+                    rightLength = height[tail];
+                } else {
+                    value = value + (rightLength - height[tail]);
+                }
+            }
 
 
-        return 1;
+        }
+        return value;
     }
 }
